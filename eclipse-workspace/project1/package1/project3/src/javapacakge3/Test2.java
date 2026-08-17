d73791513d35a044892695bc2faf16a127d97b54
kullayammabijinala <kullayammabijinala@gmail.com>
2026-08-17 12:45:20 +0530

    i added Test2 file

---
+++ eclipse-workspace/project1/package1/project3/src/javapacakge3/Test2.java
@@ -0,0 +1,13 @@
+package javapacakge3;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//LVE-local var execution
+		int a=10;
+		int b=20;
+		System.out.println(a+b);//OS-operand stack
+		System.out.println(10/0);//FD-frame data
+}
+}
